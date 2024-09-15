3b38599b3cdd0ad8cb5c9fc993aac005b8f103fb
TianTran <87957546+thienzone280596@users.noreply.github.com>
2024-09-15 15:45:17 +0700

    selected place detail

--- SwiftUI/ExampleCode/NearMe/NearMe/ContentView.swift
+++ SwiftUI/ExampleCode/NearMe/NearMe/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 import MapKit
 
+enum DisplayMode {
+  case list
+  case detail
+}
+
 struct ContentView: View {
     
     @State private var query: String = "Coffee"
@@ -17,7 +22,9 @@ struct ContentView: View {
     @State private var isSearching: Bool = false
     @State private var mapItems: [MKMapItem] = []
     @State private var visibleRegion: MKCoordinateRegion?
-    
+    @State private var selectedMapItem:MKMapItem?
+  @State private var displayMode:DisplayMode = .list
+
     private func search() async {
         
         do {
@@ -33,7 +40,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Map(position: $position) {
+          Map(position: $position,selection: $selectedMapItem) {
                 ForEach(mapItems, id: \.self) { mapItem in
                     Marker(item: mapItem)
                 }
@@ -46,10 +53,16 @@ struct ContentView: View {
                 }
             })
             .sheet(isPresented: .constant(true), content: {
-                VStack {
-                 SearchBarView(search: $query, searching: $isSearching)
-                 PlaceListView(mapItems: mapItems)
-                    Spacer()
+              VStack() {
+                  switch displayMode {
+                  case .list:
+                    SearchBarView(search: $query, searching: $isSearching)
+                    PlaceListView(mapItems: mapItems)
+                  case .detail:
+                    SelectedPlaceDetailView(mapItem: $selectedMapItem)
+                      .padding()
+                  }
+                Spacer()
                 }
                 .presentationDetents([.fraction(0.15), .medium, .large])
                 .presentationDragIndicator(.visible)
@@ -57,6 +70,13 @@ struct ContentView: View {
                 .presentationBackgroundInteraction(.enabled(upThrough: .medium))
             })
         }
+        .onChange(of: selectedMapItem, {
+          if selectedMapItem != nil {
+            displayMode = .detail
+          } else {
+            displayMode = .list
+          }
+        })
         .onMapCameraChange { context in
             visibleRegion = context.region
         }
---
+++ SwiftUI/ExampleCode/NearMe/NearMe/Views/SelectedPlaceDetailView.swift
@@ -0,0 +1,37 @@
+//
+//  SelectedPlaceDetailView.swift
+//  NearMe
+//
+//  Created by ThienTran on 15/9/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct SelectedPlaceDetailView: View {
+  @Binding var mapItem:MKMapItem?
+
+    var body: some View {
+      HStack(alignment: .top, content: {
+        VStack(alignment: .leading) {
+          if let mapItem {
+            PlaceView(mapItem: mapItem)
+          }
+        }
+        Image(systemName: "xmark.circle.fill")
+          .padding([.trailing], 10)
+          .onTapGesture {
+            mapItem = nil
+          }
+      })
+     
+    }
+}
+
+#Preview {
+  let apple =
+  Binding<MKMapItem?>(
+    get: {PreviewData.apple},
+    set: {_ in})
+ return SelectedPlaceDetailView(mapItem: apple)
+}
