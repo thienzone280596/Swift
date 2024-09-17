eceb0845c80e555c274a0b95a2b2fae5c572112c
TianTran <87957546+thienzone280596@users.noreply.github.com>
2024-09-17 17:01:29 +0700

    push all code monitoring region

---
+++ SwiftUI/ExampleCode/MapKitTutorital/MapKitTutorital/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  MapKitTutorital
+//
+//  Created by Mohammad Azam on 9/12/23.
+//
+
+import SwiftUI
+import MapKit
+
+// Apple Campus: 37.33471927772464, -122.00896992317077
+// Cupertino Village: 37.33594803602676, -122.01542259272111
+// Menlo Park: 37.45334697008818, -122.18430315050811
+
+struct ContentView: View {
+    
+    @State private var locationManager = LocationManager.shared
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    
+    let artwork = ["appleCampus": "apple-park", "cupertinoVillage": "cupertino-village"]
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            
+            Map(position: $position) {
+                UserAnnotation()
+                
+                MapCircle(center: .cupertinoVillage, radius: 50)
+                    .foregroundStyle(.indigo.opacity(0.4))
+                
+                MapCircle(center: .appleCampus, radius: 50)
+                                .foregroundStyle(.pink.opacity(0.4))
+            }
+        }
+        .sheet(item: $locationManager.locationEvent, content: { event in
+          Image(artwork[event.identifier] ?? "apple park")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+          Text(event.identifier)
+        })
+        .task {
+          await locationManager.startRegionMonitoring() 
+        }
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
